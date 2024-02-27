50292b1030516a79fbed99886a3f6c294942b4d6
Yuliia Melnyk <97669257+JuMillerDev@users.noreply.github.com>
2024-02-27 09:26:16 +0100

    created experience section

--- src/app/components/experience-section/experience-section.component.ts
+++ src/app/components/experience-section/experience-section.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experience-section.component.html',
   styleUrl: './experience-section.component.scss'
 })
 export class ExperienceSectionComponent {
+  jobTitles = [
+    { title: 'NetMaxBud', position:'Front-end developer', description: 'Description', instruments:[], clicked: true },
+    { title: 'To be continued...', position:'', description: '', instruments:[], clicked: false },
+  ];
 
+  toggleJobInfo(job: any) {
+    // Toggle active state for the clicked job
+    job.clicked = !job.clicked;
+
+    // Deactivate other jobs
+    this.jobTitles.forEach(j => {
+      if (j !== job) {
+        j.clicked = false;
+      }
+    });
+  }
 }
